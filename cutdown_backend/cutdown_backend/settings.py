6cdc6bd72579867c333868ac2b0d1d1d0b24c8e1
Nimesh Kumar <nimesh.kumar031@gmail.com>
2022-06-18 16:42:58 +0530

    Added .env

--- cutdown_backend/cutdown_backend/settings.py
+++ cutdown_backend/cutdown_backend/settings.py
@@ -11,21 +11,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#s0o$@irysx(4zl&$+vf!=%#@)9pat4b!^#puxqgo)^w%s2l+i'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 
 # Application definition
